d0050b5105a009f82d56dd6560e0e672ee06ddf6
Hayato Arai <hayato4d@gmail.com>
2024-04-07 18:28:35 +0900

    Fix comments

--- src/my_script.sh
+++ src/my_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-output_file="outside-speedtest.json"
+output_file="ospeedtest.json"
 
 # JSONファイルが存在しない場合は、空の配列を作成
 if [ ! -f $output_file ]; then
@@ -13,5 +13,5 @@ while true; do
     # 新しい結果をJSONファイルに追加
     jq --argjson result "$result" '. += [$result]' $output_file > tmp.json
     mv tmp.json $output_file
-    sleep 120 # 5分ごとに計測（秒数は適宜変更）
+    sleep 120 # 2分ごとに計測（秒数は適宜変更）
 done
